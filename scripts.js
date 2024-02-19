2e7001cf4f4d15419110b3a8cf77f32b9ba78f47
Devjo3 <bufalloxs@gmail.com>
2024-02-19 17:20:57 -0300

    carrinho

--- scripts.js
+++ scripts.js
@@ -2,3 +2,6 @@ function closeNavbar() {
     // Fecha a barra de navegação quando um item é clicado
     document.getElementById('navbarSideCollapse').click();
   }
+  function openCartModal() {
+    $('#carrinhoModal').modal('show'); // Abre o modal do carrinho
+}
